aed19590650109b186dca10f580da610080bce80
KlaudiaClaudia <claudiadabpriv@gmail.com>
2022-06-18 12:25:51 +0200

    Polaczenie z baza

--- php/config.php
+++ php/config.php
@@ -1,7 +1,7 @@
 <?php
-  $hostname = "localhost";
-  $username = "root";
-  $password = "";
+  $hostname = "comunicator-db.mysql.database.azure.com";
+  $username = "klaudia@comunicator-db";
+  $password = "&NdKpiHrU(oIR";
   $dbname = "chatapp";
 
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
